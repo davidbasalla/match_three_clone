26201b486c00c1e66c15d066e4c54c5972891d69
David Basalla <davidbasalla@gmail.com>
2018-02-17 20:15:33 +0000

    Add webpack config and run
    This probably won't work right now... Need to update from RequireJS
    to ES6 syntax

---
+++ dist/bundle.js
@@ -0,0 +1,99 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+undefined;
+
+requirejs(["underscore", 
+           "fabric",
+           "app/game",
+           "app/board",
+           "app/gem", 
+           "app/shape",
+           "app/map_parser",
+           "app/map",
+           "app/matched_gem_counter",
+           "app/gem_manipulator",
+           "app/matching_shape_finder",
+           "app/logger"], 
+           function(game, board, gem, shape, map_parser, map, matched_gem_counter, gem_manipulator, matching_shape_finder, logger) {
+  //This function is called when scripts/helper/util.js is loaded.
+  //If util.js calls define(), then this function is not fired until
+  //util's dependencies have loaded, and the util argument will hold
+  //the module value for "helper/util".
+
+  console.log('Program starts...');
+
+  var logger = new Logger(true);
+  var game = new Game(Map.map_default(), logger);
+  game.start();
+});
+
+
+/***/ })
+/******/ ]);
