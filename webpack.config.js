1e09beddae195bf550f479f102d06337f5c8e4c4
David Basalla <davidbasalla@gmail.com>
2018-02-18 15:22:39 +0000

    Split out vendor chunk with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    vendor: ['lodash', 'fabric'],
+    app: './src/app.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      // filename: "vendor.js"
+      // (Give the chunk a different name)
+  
+      minChunks: Infinity,
+      // (with more entries, this ensures that no other module
+      //  goes into the vendor chunk)
+    })
+  ]
 };
